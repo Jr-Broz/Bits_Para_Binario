d8c9847f67e38996a4564f2566a30bfc7953ccf7
João Rafael Broz dos Santos <127759756+Jr-Broz@users.noreply.github.com>
2023-08-30 10:59:52 -0300

    uploaded Program.cs

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using System;
+namespace Program{
+
+    public class Program{
+
+        public static void Main(String[] args ){
+
+
+            ParaBinario pbin = new ParaBinario();
+
+            pbin.Int_Para_Binario();
+
+
+            pbin.string_Para_Binario();
+
+        }
+    }
+}
